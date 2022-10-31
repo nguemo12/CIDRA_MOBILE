188a7ccaae6c93917d6074b3a870450300e1e978
Peliah <pelepoupa@gmail.com>
2022-10-31 16:22:58 +0100

    form validation good. modified welcome ui

--- src/AuthScreeen/Welcome/WelcomeScreen.js
+++ src/AuthScreeen/Welcome/WelcomeScreen.js
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable quotes */
+/* eslint-disable semi */
 import React from "react";
 import WelcomeStyle from "./WelcomeStyle";
 import { View, Text, Image, TouchableOpacity } from "react-native"
@@ -14,8 +18,8 @@ const WelcomeScreen = ({navigation}) => {
                 <Image source={logo} style={{ width: "47%", height: "40%" , alignSelf:"center", borderRadius:50, resizeMode:"cover", marginTop:"3%" }} />
                 <Text style={{ textAlign: "center", fontSize: 40, fontWeight: "bold", marginTop: "10%", color: "#003D79" }}> Welcome to Cidra </Text>
                 <Text style={{ textAlign: "center", color: "#003D79" }}> Your MR Medical healthCare assistant</Text>
-                <View style={{ width: "90%", height: "15%", flexDirection: "row", justifyContent: "space-around" }}>
-                    <TouchableOpacity style={{ padding: 16, width: "60%", marginTop: "10%", height: "80%", backgroundColor: "#379AE1", borderRadius: 50 }}><Text style={{ color: "white", textAlign: "center", fontSize: 20, fontWeight: "bold" }} onPress = {()=> navigation.push("login")}>Get Started</Text></TouchableOpacity>
+                <View style={{ width: "100%", flexDirection: "row", justifyContent: "space-around", paddingTop:10}}>
+                    <TouchableOpacity style={{width: "50%", height: "100%", backgroundColor: "#379AE1", borderRadius: 50, alignItems: 'center', marginTop: 20,}}><Text style={{ color: "white", textAlign: "center", fontSize: 20, fontWeight: "bold", marginTop: 22}} onPress = {()=> navigation.push("login")}>Get Started</Text></TouchableOpacity>
 
                 </View>
             </View>
