848ae7b683004991c8f4b712e11584f45ac587af
Aymard <yakubaba1234@gmail.com>
2022-11-07 16:46:41 +0100

    added form validation

--- src/AuthScreeen/LoginScreen/LoginScreen.js
+++ src/AuthScreeen/LoginScreen/LoginScreen.js
@@ -21,26 +21,27 @@ if (Platform.OS === 'android') {
 
 
 const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [message, setMessage] = useState('')
-
-    const login = async (email, password) => {
-
-        //axios POST request
-
-    }
-
-    const submitForm = async (email, password) => {
-        if (!email || !password) {
-            setMessage('Invalid username/password')
-            setTimeout(() => {
-                setMessage('')
-            }, 2000)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isEmailEmpty, setIsEmailEmpty] = useState(false)
+    const [isPasswordEmpty, setIsPasswordEmpty] = useState(false)
+    const checkEmptyText = () => {
+        if (email == "") {
+            setIsEmailEmpty(true)
+            return
         }
+       
+        if (password == "") {
+            setIsPasswordEmpty(true)
+            return
+        }
+      
 
-        login(email, password)
+        navigation.navigate("BottomNav")
     }
+
+   
+    
     return (
         <View style={{ flex: 1, backgroundColor: "#379AE1" }}>
             <View style={{ width: "100%", height: "25%", backgroundColor: "#379AE1", flexDirection: "row" }}>
@@ -55,20 +56,32 @@ const LoginScreen = ({ navigation }) => {
                     <Text style={{ color: "black", fontWeight: "bold", fontSize: 20, marginTop: "2%", marginLeft: "5%" }} >Welcome Back !</Text>
                     <Text style={{ color: "black", fontWeight: "bold", fontSize: 20, marginLeft: "5%" }} >You've been missed</Text>
                     <Text style={{ color: "grey", fontSize: 20, marginTop: "2%", marginLeft: "5%" }} >Please log-in to proceed</Text>
+                    {
+                        isEmailEmpty ?
+                            <Text style={{ display: "flex", color: '#F8AE1E', marginLeft: 270, marginTop: 20 }}>*required</Text>
+                            : null
+                    }
                     <TextInput
                         placeholder="Email"
                         placeholderTextColor={"grey"}
+                       onChange ={(text)=>setEmail(text)}
                         value={email}
-                        style={{ borderWidth: 1, width: "90%", height: "10%", borderRadius: 5, marginTop: "8%", marginLeft: "5%", padding: 16, color: "black" }}
+                        style={{ borderWidth: 1, width: "90%", height: "10%", borderRadius: 5, marginTop: "5%", marginLeft: "5%", padding: 16, color: "black" }}
 
                     />
+                     {
+                        isPasswordEmpty ?
+                            <Text style={{ display: "flex", color: '#F8AE1E', marginLeft: 270, marginTop: 20 }}>*required</Text>
+                            : null
+                    }
                     <View style={{ borderWidth: 1, width: "90%", height: "10%", borderRadius: 5, padding: 8, marginTop: "10%", marginLeft: "5%", flexDirection: "row", justifyContent: "space-around" }}>
                         <TextInput
                             placeholder="password"
                             placeholderTextColor={"grey"}
+                            onChange ={(text)=>setPassword(text)}
+                            value={password}
                             style={{ width: "90%", height: "100%", color: "black" }}
                             secureTextEntry={true}
-                            value={password}
                         />
                         <Image source={eyeoff} style={{ width: "10%", height: "60%" }} />
 
@@ -76,7 +89,7 @@ const LoginScreen = ({ navigation }) => {
                     <Text style={{ alignSelf: "flex-end", fontSize: 20, marginVertical: "2%", fontWeight: "bold", color: "black", marginRight: "5%" }} onPress={() => navigation.navigate("forgotpass")}>Forgot Password ?</Text>
 
                     <View style={{ width: "90%", height: "10%", flexDirection: "row", justifyContent: "space-around" }}>
-                        <TouchableOpacity style={{ padding: 8, width: "60%", height: "100%", backgroundColor: "#379AE1", borderRadius: 50 }}><Text style={{ color: "white", textAlign: "center", fontSize: 20, fontWeight: "bold" }} onPress={() => navigation.navigate("BottomNav") } >Login</Text></TouchableOpacity>
+                        <TouchableOpacity style={{ padding: 8, width: "60%", height: "100%", backgroundColor: "#379AE1", borderRadius: 50 }}><Text style={{ color: "white", textAlign: "center", fontSize: 20, fontWeight: "bold" }} onPress={() => checkEmptyText() } >Login</Text></TouchableOpacity>
                         <TouchableOpacity style={{ padding: 8, width: "35%", height: "100%", backgroundColor: "#003D79", borderRadius: 50, flexDirection: "row", justifyContent: "space-between" }}><Text style={{ color: "white", fontSize: 12 }} onPress={() => navigation.navigate("fingerprint")}>or with {"\n"} fingerprint</Text>
                             <Image source={finger} style={{ width: "100%", height: "100%", alignSelf: "center" }} />
                         </TouchableOpacity>
